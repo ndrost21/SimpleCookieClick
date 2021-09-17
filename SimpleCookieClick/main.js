fa6f79cc20499e22894941936e36d36199ab5896
Nick <ndrost@gmail.com>
2021-09-17 08:42:42 -0400

    Update main.js

--- SimpleCookieClick/main.js
+++ SimpleCookieClick/main.js
@@ -1,18 +1,14 @@
-Game.registerMod("simple sookie click",{
-	init:function(){
-	Game.Notify(`Simple Cookie Click!`,'',[16,5]);
-		var autoClickGolden = setInterval(function() {
-			for (var h in Game.shimmers) {
-				if(Game.shimmers[h].type=="golden") {
-					Game.shimmers[h].pop();
-				}
-			} 
-		} , 1000);	
-	},
-	save:function(){
-	},
-	load:function(str){
-	},
+Game.registerMod("simple sookie click", {
+  init: function () {
+    Game.Notify(`Simple Cookie Click!`, "", [16, 5]);
+    var autoClickGolden = setInterval(function () {
+      for (var h in Game.shimmers) {
+        if (Game.shimmers[h].type == "golden") {
+          Game.shimmers[h].pop();
+        }
+      }
+    }, 1000);
+  },
+  save: function () {},
+  load: function (str) {},
 });
-
-   
