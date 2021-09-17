48f67e4777b4a022f9c5fe9941d5475c408e306c
Nick <ndrost@gmail.com>
2021-09-17 11:42:53 -0400

    first stab at autoSpell Casting

--- SimpleCookieClick/main.js
+++ SimpleCookieClick/main.js
@@ -1,8 +1,8 @@
 Game.registerMod("simple cookie click", {
   init: function () {
     Game.Notify(`Simple Cookie Click Loaded!`, "", [16, 5]);
-    
-	var autoClickGolden = setInterval(function () {
+
+    var autoClickGolden = setInterval(function () {
       for (var h in Game.shimmers) {
         if (Game.shimmers[h].type == "golden") {
           Game.shimmers[h].pop();
@@ -11,6 +11,22 @@ Game.registerMod("simple cookie click", {
     }, 1000);
 
     var autoClickBigCookie = setInterval(Game.ClickCookie, 100);
+
+    var autoSpellCast = setInterval(
+      function () {
+        var grimoireObject = Game.ObjectsById[7].minigame;
+
+        if (
+          Game.shimmers.legnth == 0 &&
+          grimoireObject.magicM >= 10 + 0.1 * grimoireObject.magicM &&
+          grimoireObject.magic == grimoireObject.magicM
+        ) {
+          grimoireObject.castSpell(grimoireObject.spellsById[1], {});
+        }
+      },
+
+      1000
+    );
   },
   save: function () {},
   load: function (str) {},
