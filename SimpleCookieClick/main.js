2a27c324c24cdba66f3a3787bb29ddc19bbc400a
Nick <ndrost@gmail.com>
2021-09-17 09:25:12 -0400

    Update main.js

--- SimpleCookieClick/main.js
+++ SimpleCookieClick/main.js
@@ -1,13 +1,16 @@
-Game.registerMod("simple sookie click", {
+Game.registerMod("simple cookie click", {
   init: function () {
-    Game.Notify(`Simple Cookie Click!`, "", [16, 5]);
-    var autoClickGolden = setInterval(function () {
+    Game.Notify(`Simple Cookie Click Loaded!`, "", [16, 5]);
+    
+	var autoClickGolden = setInterval(function () {
       for (var h in Game.shimmers) {
         if (Game.shimmers[h].type == "golden") {
           Game.shimmers[h].pop();
         }
       }
     }, 1000);
+
+    var autoClickBigCookie = setInterval(Game.ClickCookie, 500);
   },
   save: function () {},
   load: function (str) {},
